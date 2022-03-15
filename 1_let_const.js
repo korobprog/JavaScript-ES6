f7aac6ff8ad822605d09f60619ffd9671868af75
Max <korobprog@gmail.com>
2022-03-15 10:54:41 +1000

    1_let_const

--- 1_let_const.js
+++ 1_let_const.js
@@ -1,7 +1,47 @@
-let a = 24
+// let  a = 24
 
-if (true) {
-    let a = 42
-}
+// if (true) {
+//     let a = 42
 
-console.log(a)
+//     console.log('a' , a)
+// }
+
+// console.log(a)
+
+// Hosting
+
+// b = 20
+
+// console.log(b)
+
+// let b = 10
+
+// function hoisted {
+//   age = 26  
+// }
+
+// let age
+// hoisted()
+// console.log(age)
+
+// Const
+
+const COLOR = '#ffebb'
+
+// color = '#000'
+
+// console.log(COLOR)
+
+// const array = [1, 2, 3, 5, 8]
+
+// console.log(array)
+
+// array.push(13) //добавить в масив
+
+// console.log(array)
+
+
+const obj = {a: 42}
+obj.name = 'Igor'
+
+console.log(obj)
